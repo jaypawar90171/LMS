fcee529461ff339f7eb15484bf16e8694f455830
jaypawar90171 <jayp90171@gmail.com>
2025-11-17 15:16:14 +0530

    2025-11-17 15:16

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,10 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import multer from 'multer'
+import dotenv from 'dotenv'
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: "dorgm2sje",
-  api_key: "968116822669483",
-  api_secret: "Z6P0u2ovIGMEzrzOPkZapfj252g",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export const uploadFile = async (filePath: any) => {
